9f99b4a94ee2c88dbb5cd99e2bc96a8b885947d5
serhaturuc-hash <serhat.uruc@ogr.sakarya.edu.tr>
2026-05-10 18:36:10 +0300

    Create login.php
    login bölümünü tamamladık

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>Giriş Yap</h1>
+</header>
+
+<section>
+
+<form action="kontrol.php" method="POST">
+
+<label>Email:</label>
+<br>
+<input type="text" name="email">
+<br><br>
+
+<label>Şifre:</label>
+<br>
+<input type="password" name="password">
+<br><br>
+
+<button type="submit">
+Giriş Yap
+</button>
+
+</form>
+
+</section>
+
+</body>
+</html>
+
